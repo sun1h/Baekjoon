9a810cbe6725818c9ec2fa50f49740ed33ee7ad8
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-02-22 23:22:37 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     #numbers.sort()
-    answer = -20000000000000
+    answer = -10000*10000
     for i in range(len(numbers)-1):
         for j in range(i+1,len(numbers)):
             if numbers[i]*numbers[j]  >= answer:
