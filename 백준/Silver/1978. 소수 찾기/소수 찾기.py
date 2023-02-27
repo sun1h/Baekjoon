940e66f3ef78f77d56cd11ec1445402c3c2f2a1c
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-02-28 07:55:49 +0900

    [Silver V] Title: 소수 찾기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -3,13 +3,11 @@ arr=list(map(int,input().split()))
 tmp=0
 total=0
 for a in arr:
+    tmp=0
     for i in range(1,a):   
         if a % i ==0:
             tmp +=1
     if tmp == 1:
         total+=1
-        tmp=0
-    else: 
-        tmp=0
 print(total)
 
