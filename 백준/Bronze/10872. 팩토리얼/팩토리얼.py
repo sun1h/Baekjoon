41d3a279845cae11de864e580950f22c00fce489
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-03-01 21:55:03 +0900

    [Bronze V] Title: 팩토리얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,5 +1,11 @@
+def f(n):
+    global ans
+    if n == 0:
+        return 1
+    if n == 1:
+        return ans
+    ans=ans*n
+    return f(n-1)    
 N=int(input())
 ans=1
-for i in range(2,N+1):
-    ans*=i
-print(ans)
+print(f(N))
