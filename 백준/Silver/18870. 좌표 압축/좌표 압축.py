20afc9f377c709146b912bdff898af3b01170204
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-03-01 21:48:51 +0900

    [Silver II] Title: 좌표 압축, Time: 1500 ms, Memory: 293376 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,13 +1,8 @@
 N=int(input())
 arr=list(map(int,input().split()))
-
 a=sorted(list(set(arr)))
-# for ar in arr:
-#     print(a.index(ar),end=' ')
 
-numdict = {}
-for i in range(len(a)):
-    numdict[a[i]] = i
+dic={x: i for i,x in enumerate(a)}
 
-for i in arr:
-    print(numdict[i], end=' ')
+for ar in arr:
+    print(dic[ar], end=' ')
