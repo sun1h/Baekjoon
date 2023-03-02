e61cc846872cdf662000ac3ea6b08d89378147af
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-03-03 05:39:40 +0900

    [Silver V] Title: 줄세우기, Time: 176 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -0,0 +1,10 @@
+P=int(input())
+for _ in range(P):
+    arr=list(map(int,input().split())) 
+    total=0
+    for i in range(1,len(arr)-1):  
+        for j in range(i+1,len(arr)): # i 뒤에 애들과 전부 비교해서
+            if arr[i] > arr[j]:  # i가 더 크면
+                arr[i],arr[j] = arr[j],arr[i]  # 자리바꾸기
+                total+=1
+    print(arr[0], total)
