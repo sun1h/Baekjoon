034f3d11ff70362d44199fe20f08b36798384ee2
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-03-20 23:11:06 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 115176 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,17 +1,14 @@
-k,n= map(int, input().split())
-line = []
-for _ in range(k):
-    line.append(int(input()))
-start = 1
-end = max(line)
- 
-while(start<=end):
-    mid = (start+end)//2
-    cnt = 0
-    for i in line:
-        cnt += i//mid
-    if cnt>=n: #자른 개수가 많으면 -> 더 크게잘라야함
-        start = mid+1
+K,N=map(int,input().split())
+lst=[int(input()) for _ in range(K)]
+start,end=1,max(lst) # 1을 시작, 최댓값을 끝
+while start <= end:  # 반복문 종료 조건
+    mid=(start+end)//2 # 중앙 원소 고르기
+    total=0 # 랜선 갯수
+    for l in lst:
+        total += l // mid # 중앙값 만큼 랜선을 자르면 얻을 수 있는 최대 갯수 = 랜선길이 // 중앙값
+    if total >= N:
+        start=mid+1
     else:
-        end = mid-1
+        end= mid-1
 print(end)
+
