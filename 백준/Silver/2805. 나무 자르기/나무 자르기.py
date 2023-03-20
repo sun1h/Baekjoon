b9a544e384655d0a175887f37b27bf0e19f12166
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-03-20 21:46:53 +0900

    [Silver II] Title: 나무 자르기, Time: 4440 ms, Memory: 150704 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,18 +1,16 @@
 N, M = map(int, input().split())
-tree = list(map(int, input().split()))
-start, end = 1, max(tree) #이분탐색 검색 범위 설정
+h = list(map(int, input().split()))
+start, end = 1, max(h)  # 1을 시작, 최댓값을 끝
 
-while start <= end: #적절한 벌목 높이를 찾는 알고리즘
-    mid = (start+end) // 2
-    
-    log = 0 #벌목된 나무 총합
-    for i in tree:
-        if i >= mid:
-            log += i - mid
-    
-    #벌목 높이를 이분탐색
-    if log >= M:
-        start = mid + 1
+while start <= end:
+    mid = (start + end) // 2
+    total = 0  # 잘린 나무 높이 총합
+    for H in h:
+        if H > mid:
+            total += H - mid
+    #이진탐색
+    if total >= M:
+        start = mid+1 # 잘린 나무 높이 총함이 M이상 이면 중앙값+1 ~ 끝 값 다시 찾기
     else:
-        end = mid - 1
+        end = mid-1 # 잘린 나무 높이 총함이 M미만 이면 시작-1 ~ 중앙값 값 다시 찾기
 print(end)
