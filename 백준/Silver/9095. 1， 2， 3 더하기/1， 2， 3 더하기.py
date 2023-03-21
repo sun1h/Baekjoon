441785eefbc3af04c1787349b8ead0ba09bf1073
sun_1 <124047574+sun1h@users.noreply.github.com>
2023-03-22 00:11:40 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,18 +1,19 @@
-T = int(input())
-
-for i in range(T) :
-    n = int(input())
-    dp = [0]*(n+1)
-    if n == 1 :
-        print(1)
-    elif n == 2 :
-        print(2)
-    elif n == 3 :
-        print(4)
-    else :
-        dp[1] = 1
+def find(n):
+    dp = [0] * (n + 1)
+    dp[1] = 1  # 1
+    if n ==2:
+        dp[2] = 2  # 2 / 1+1
+    if n==3:
+        dp[2] = 2
+        dp[3] = 4  # 3/ 1+2 /1+1+1/ 2+1
+    if n > 3:
         dp[2] = 2
         dp[3] = 4
-        for j in range(4,n+1) :
-            dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
-        print(dp[n])
+        for i in range(4, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    return dp[n]
+
+T = int(input())
+for i in range(T):
+    n = int(input())
+    print(find(n))
