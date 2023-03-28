86db378d595eef1b96a8ecd003f5842be2f31313
sun._.1 <124047574+sun1h@users.noreply.github.com>
2023-03-28 10:33:59 +0900

    [Gold V] Title: 평범한 배낭, Time: 412 ms, Memory: 194320 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,18 @@
-n, k = map(int, input().split())
+N,K=map(int,input().split())
+lst=[[0, 0]]
+for _ in range(N):
+    lst.append(list(map(int, input().split())))
+dp=[[0]*(K+1) for _ in range(N+1)]
 
-thing = [[0,0]]
-d = [[0]*(k+1) for _ in range(n+1)]
+for i in range(1,N+1):
+    for j in range(1,K+1):
+        weight = lst[i][0]
+        value = lst[i][1]
+        # 현재 배낭의 허용 무게보다 넣을 물건의 무게가 더 크다면 넣지 않는다.
+        if j < weight:
+            dp[i][j]=dp[i-1][j]
+        else: # 그렇지 않다면, 다음 중 더 나은 가치를 선택하여 넣는다
+            # 현재 물건을 넣지않고 이전 배낭 그대로 가지고 간다. vs 현재 넣을 물건의 무게만큼 배낭에서 뺀다. 그리고 현재 물건을 넣는다.
+            dp[i][j]=max(dp[i-1][j], dp[i-1][j-weight]+value)
 
-for i in range(n):
-    thing.append(list(map(int, input().split())))
-
-for i in range(1, n+1):
-    for j in range(1, k+1):
-        w = thing[i][0]
-        v = thing[i][1]
-
-        if j < w:
-            d[i][j] = d[i-1][j]
-        else:
-            d[i][j] = max(d[i-1][j], d[i-1][j-w]+v)
-
-print(d[n][k])
+print(dp[N][K])
