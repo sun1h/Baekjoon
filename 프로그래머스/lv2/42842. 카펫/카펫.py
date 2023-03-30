ff675053f16106c5aa4ab42fcb387414c2c6e1ad
sun._.1 <124047574+sun1h@users.noreply.github.com>
2023-03-30 11:48:00 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,7 @@
+def solution(brown, yellow):
+    tmp=brown+yellow
+    answer = 0
+    for i in range(1,max(brown,yellow)):
+        if yellow/i+i==brown/2-2:   
+            answer=[i+2,tmp/(i+2)]
+    return answer
