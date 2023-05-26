4d4c4d1d14f4c92757c1ec9cb577fb060f4b669a
sun._.1 <124047574+sun1h@users.noreply.github.com>
2023-05-26 14:48:18 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 162.06 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -0,0 +1,34 @@
+import sys
+
+def solution(rows, columns, queries):
+    arr = [[(i)*columns+(j+1) for j in range(columns)] for i in range(rows)]
+    result = []
+    for x1, y1, x2, y2 in queries:
+        result.append(change(x1-1, y1-1, x2-1, y2-1, arr))
+    return result
+
+
+def change(x1, y1, x2, y2, arr):
+    min_value = sys.maxsize
+    # 맨 위의 맨 왼쪽 값을 기록
+    temp = arr[x1][y1]
+    # 왼쪽
+    for k in range(x1, x2):
+        arr[k][y1] = arr[k+1][y1]
+        min_value = min(min_value, arr[k+1][y1])
+    # 아래
+    for k in range(y1, y2):
+        arr[x2][k] = arr[x2][k+1]
+        min_value = min(min_value, arr[x2][k+1])
+    # 오른쪽
+    for k in range(x2, x1, -1):
+        arr[k][y2] = arr[k-1][y2]
+        min_value = min(min_value, arr[k-1][y2])
+    # 위
+    for k in range(y2, y1+1, -1):
+        arr[x1][k] = arr[x1][k-1]
+        min_value = min(min_value, arr[x1][k-1])
+    # 기록했던 값 업데이트
+    arr[x1][y1+1] = temp
+    min_value = min(min_value, temp)
+    return min_value
