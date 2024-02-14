5a0ff84bf82dfbadd2ed9bf5c683522c45980171
sun._.1 <124047574+sun1h@users.noreply.github.com>
2024-02-14 10:02:10 +0900

    [level 0] Title: x 사이의 개수, Time: 3.48 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -3,16 +3,10 @@ import java.util.*;
 class Solution {
     public ArrayList solution(String myString) {
         ArrayList answer = new ArrayList<>();
-        myString+="Xx";
-        String[] tmp = myString.split("x");
+        String[] tmp = myString.split("x",myString.length());
         
         for(int i=0; i<tmp.length; i++){
-            if(tmp[i].contains("X")){
-                answer.add(tmp[i].length()-1);
-                break;
-            }else{
-                answer.add(tmp[i].length());
-            }
+            answer.add(tmp[i].length());
         }
         return answer;
     }
