74cad35b3aabe55d8ec327d0a2f0fb6e617401ac
sun._.1 <124047574+sun1h@users.noreply.github.com>
2024-02-26 15:29:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num) {
+        long n = (long) num;
+        int answer = 0;
+        while(n!=1){
+            if(answer==500){
+                answer = -1;
+                break;
+            }else if(n%2==0){
+                n/=2;
+            }else{
+                n=n*3+1;
+            }
+            answer+=1;
+        }
+        return answer;
+    }
+}
