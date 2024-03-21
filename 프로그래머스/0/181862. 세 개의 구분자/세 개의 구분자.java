5c8ab9f7bed379309adc0ed00756c3df571e2cb1
sun._.1 <124047574+sun1h@users.noreply.github.com>
2024-03-21 11:10:05 +0900

    [level 0] Title: 세 개의 구분자, Time: 45.58 ms, Memory: 96.9 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,19 +3,12 @@ import java.util.*;
 class Solution {
     public ArrayList solution(String myStr) {
         ArrayList answer = new ArrayList<>();
-        String tmp = "";
-        for(char c:myStr.toCharArray()){
-            if(c=='a'||c=='b'||c=='c'){
-                if(tmp.length()!=0){
-                    answer.add(tmp);
-                    tmp = "";
-                }
-            }else{
-                tmp+=c;
+        String[] splitStr = myStr.split("a|b|c");  
+
+        for (String s : splitStr) {
+            if (!s.isEmpty()) {
+                answer.add(s);
             }
-        }
-        if(tmp.length()!=0){
-            answer.add(tmp);
         }
         if(answer.size()==0){
             answer.add("EMPTY");
