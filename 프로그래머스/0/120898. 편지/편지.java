2809c752a01475d78e091a2e951cc61d8a644670
sun._.1 <124047574+sun1h@users.noreply.github.com>
2024-10-24 13:15:58 +0900

    [level 0] Title: 편지, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = message.length()*2;
+        // for(int i=1; i<= message.length(); i++){
+        //     answer+=2;
+        // }
+        return answer;
+    }
+}
