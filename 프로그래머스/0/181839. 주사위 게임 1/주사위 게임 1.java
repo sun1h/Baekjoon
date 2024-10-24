acf2537b0e5aff536e1e042580f92dca084a2966
sun._.1 <124047574+sun1h@users.noreply.github.com>
2024-10-24 16:13:45 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if((a+b)%2!=0){
+            answer = 2*(a+b);
+        }else if(a%2!=0){
+            answer = a*a+b*b;
+        }else{
+            answer = Math.abs(a-b);
+        }
+        return answer;
+    }
+}
