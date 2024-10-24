15d676c10f5abde5e0e97fc6a99a521223a4b1e4
sun._.1 <124047574+sun1h@users.noreply.github.com>
2024-10-24 14:07:41 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.40 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862?language=java) 
 
 ### 성능 요약
 
-메모리: 64.8 MB, 시간: 0.02 ms
+메모리: 73.5 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 14:26:40
+2024년 10월 24일 14:07:39
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,15 +1,19 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = Integer.MIN_VALUE;
-        int tmp =0;
-        for(int i=0; i<numbers.length-1; i++){
-            for(int j=i+1; j<numbers.length ;j++){
-                tmp = numbers[i]*numbers[j];
-                if(answer < tmp){
-                    answer = tmp;
-                }
+        int answer =  Integer.MIN_VALUE; 
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                answer = findMax(answer, numbers[i], numbers[j]); // answer 업데이트
             }
         }
         return answer;
+    }
+    
+    public int findMax(int currentMax, int num1, int num2) {
+        int mul = num1 * num2;
+        if (mul > currentMax) {
+            return mul; // 최대값 반환
+        }
+        return currentMax; // 최대값 유지
     }
 }
