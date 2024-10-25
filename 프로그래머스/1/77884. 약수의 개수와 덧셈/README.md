3e57ff86d4395c6d73be9c358ee196ab01054e49
sun._.1 <124047574+sun1h@users.noreply.github.com>
2024-10-25 20:24:33 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.11 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 개수와 덧셈 - 77884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77884?language=java) 
 
 ### 성능 요약
 
-메모리: 69.4 MB, 시간: 0.49 ms
+메모리: 71.5 MB, 시간: 4.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 16:32:43
+2024년 10월 25일 20:24:31
 
 ### 문제 설명
 
--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,22 +1,20 @@
-import java.util.*;
-
 class Solution {
+    private int answer = 0;
+    
     public int solution(int left, int right) {
-        int answer = 0;
         for(int i=left; i<=right; i++){
-            ArrayList numbers = new ArrayList<>();
-            for(int j=1; j<=i;j++){
-                if(i%j==0){
-                    numbers.add(j);
-                }
-            }
-            if(numbers.size()%2==0){
-                answer+=i;
-            }else{
-                answer-=i;
+            cal(i);
+        }
+        return answer;
+    }
+    
+    public void cal(int n){
+        int tmp = 1;
+        for(int i=2; i<=n; i++){
+            if(n%i==0){
+                ++tmp;
             }
         }
-        
-        return answer;
+        answer += tmp%2==0? n: -n;
     }
 }
